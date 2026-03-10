f0c072c181eb4b470852a99cf0fd05d8556543ec
Kishore-Balasubramani <kishorebalasubramani.2006@gmail.com>
2026-03-10 09:01:02 +0530

    UC 1 - Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,7 @@
 public class BookMyStayApp {
+    public static void main(String args[]){
+        System.out.println("Welcome to my BookMyStayApp");
+        System.out.println("System initialized successfully");
+        System.out.println("version : 1.0");
+    }
 }
