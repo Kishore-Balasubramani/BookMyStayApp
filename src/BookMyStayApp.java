95f800ea634e47a25de019307358ad28c99d01ad
ds9751-creator <ds9751@srmist.edu.in>
2026-03-17 09:08:37 +0530

    UC4 - Room Search & Availability Check

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,81 +1,83 @@
+//UC4
 import java.util.HashMap;
 import java.util.Map;
+
 public class BookMyStayApp {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
+
         System.out.println("Welcome to my BookMyStayApp");
-        System.out.println("System initialized successfully");
-        System.out.println("version : 1.0");
+        System.out.println("Version : 4.0 (Room Search)\n");
 
-        boolean singleRoomAvailable = true;
-        boolean doubleRoomAvailable = false;
-        boolean suiteRoomAvailable = true;
-
-
+        // Create Room Objects
         Room singleRoom = new singleRoom();
         Room doubleRoom = new doubleRoom();
         Room suiteRoom = new suiteRoom();
 
-        System.out.println("Single Room Details:");
-        singleRoom.displayRoomDetails(singleRoomAvailable);
-
-        System.out.println("Double Room Details:");
-        doubleRoom.displayRoomDetails(doubleRoomAvailable);
-
-        System.out.println("Suite Room Details:");
-        suiteRoom.displayRoomDetails(suiteRoomAvailable);
-
-        System.out.println("Application Terminated.");
-
+        // Create Inventory
         RoomInventory inventory = new RoomInventory();
 
-        Map<String, Integer> rooms = inventory.getRoomAvailability();
+        // Create Search Service
+        RoomSearchService searchService = new RoomSearchService();
 
-        for (String roomType : rooms.keySet()) {
-            System.out.println(roomType + " : " + rooms.get(roomType));
-        }
+        // UC4: Read-only Search
+        searchService.searchAvailableRooms(
+                inventory,
+                singleRoom,
+                doubleRoom,
+                suiteRoom
+        );
 
-        inventory.updateAvailability("DoubleRoom", 4);
-
-        System.out.println("Updated Availability");
-
-        for (String roomType : rooms.keySet()) {
-            System.out.println(roomType + " : " + rooms.get(roomType));
-        }
+        System.out.println("Search completed.");
     }
 }
-abstract class Room{
+
+///////////////////////////////////////////////////////////
+// ROOM CLASSES
+///////////////////////////////////////////////////////////
+
+abstract class Room {
     protected int NumberOfBeds;
     protected int squareFeet;
     protected double PricePerNight;
 
-    public Room(int NumberOfBeds,int squareFeet,double PricePerNight){
-        this.NumberOfBeds = NumberOfBeds;
-        this.squareFeet =  squareFeet;
-        this.PricePerNight = PricePerNight;
+    public Room(int beds, int size, double price) {
+        this.NumberOfBeds = beds;
+        this.squareFeet = size;
+        this.PricePerNight = price;
     }
-    public void displayRoomDetails(boolean isAvailable){
-        System.out.println("Number of Beds: " + NumberOfBeds);
-        System.out.println("squareFeet: " + squareFeet);
-        System.out.println("PricePerNight: " + PricePerNight);
-        System.out.println("isAvailable: " + isAvailable);
-        System.out.println("----------------------------");
-    }
-}
-class singleRoom extends Room{
-    public singleRoom(){
-        super(1,200,1200);
+
+    public void displayRoomDetails(boolean isAvailable) {
+        System.out.println("Beds: " + NumberOfBeds);
+        System.out.println("Size: " + squareFeet);
+        System.out.println("Price: " + PricePerNight);
+        System.out.println("Available: " + isAvailable);
+        System.out.println("------------------------");
     }
 }
-class doubleRoom extends Room{
-    public doubleRoom(){
-        super(2,400,2400);
+
+class singleRoom extends Room {
+    public singleRoom() {
+        super(1, 200, 1200);
     }
 }
-class suiteRoom extends Room{
-    public suiteRoom(){
-        super(1,500,3200);
+
+class doubleRoom extends Room {
+    public doubleRoom() {
+        super(2, 400, 2400);
     }
 }
+
+class suiteRoom extends Room {
+    public suiteRoom() {
+        super(1, 500, 3200);
+    }
+}
+
+///////////////////////////////////////////////////////////
+// INVENTORY (READ ONLY)
+///////////////////////////////////////////////////////////
+
 class RoomInventory {
 
     private Map<String, Integer> roomAvailability;
@@ -86,16 +88,49 @@ class RoomInventory {
     }
 
     private void initializeInventory() {
-        roomAvailability.put("SingleRoom", 10);
-        roomAvailability.put("DoubleRoom", 5);
-        roomAvailability.put("SuiteRoom", 2);
+        // FIXED KEYS
+        roomAvailability.put("Single", 10);
+        roomAvailability.put("Double", 0);
+        roomAvailability.put("Suite", 2);
     }
 
     public Map<String, Integer> getRoomAvailability() {
         return roomAvailability;
     }
+}
 
-    public void updateAvailability(String roomType, int count) {
-        roomAvailability.put(roomType, count);
+///////////////////////////////////////////////////////////
+// SEARCH SERVICE (UC4)
+///////////////////////////////////////////////////////////
+
+class RoomSearchService {
+
+    public void searchAvailableRooms(
+            RoomInventory inventory,
+            Room singleRoom,
+            Room doubleRoom,
+            Room suiteRoom) {
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        System.out.println("Available Rooms:\n");
+
+        Integer single = availability.get("Single");
+        if (single != null && single > 0) {
+            System.out.println("Single Room Available: " + single);
+            singleRoom.displayRoomDetails(true);
+        }
+
+        Integer dbl = availability.get("Double");
+        if (dbl != null && dbl > 0) {
+            System.out.println("Double Room Available: " + dbl);
+            doubleRoom.displayRoomDetails(true);
+        }
+
+        Integer suite = availability.get("Suite");
+        if (suite != null && suite > 0) {
+            System.out.println("Suite Room Available: " + suite);
+            suiteRoom.displayRoomDetails(true);
+        }
     }
 }
